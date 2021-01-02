27e18d0dec15e3433e64c133c8f8b3b5bfd08a23
ramkumararvind <ramkumar.arvind@gaiansolutions.com>
2021-01-02 13:22:31 +0530

    Added Prime like Site

---
+++ scripts/menu.js
@@ -0,0 +1,43 @@
+const elements = [
+    {
+        "telugu": [
+            {
+                "title": "KGF",
+                "description": null,
+                "image": "images/h.png",
+                "src": "videos/KGF.mp4"
+            },
+            {
+                "title": "KGF",
+                "description": null,
+                "image": "images/h.png",
+                "src": "videos/KGF.mp4"
+            },{
+                "title": "KGF",
+                "description": null,
+                "image": "images/h.png",
+                "src": "videos/KGF.mp4"
+            },{
+                "title": "KGF",
+                "description": null,
+                "image": "images/h.png",
+                "src": "videos/KGF.mp4"
+            },{
+                "title": "KGF",
+                "description": null,
+                "image": "images/h.png",
+                "src": "videos/KGF.mp4"
+            },{
+                "title": "KGF",
+                "description": null,
+                "image": "images/h.png",
+                "src": "videos/KGF.mp4"
+            },{
+                "title": "KGF",
+                "description": null,
+                "image": "images/h.png",
+                "src": "videos/KGF.mp4"
+            }
+        ]
+    }
+]
